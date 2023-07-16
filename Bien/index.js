94b7712026aa7131cb86fb9fd235b4d2b72746ce
vulinh1704 <truongdangvulinh1704@gmail.com>
2023-07-16 15:38:53 +0700

    variable

---
+++ Bien/index.js
@@ -0,0 +1,51 @@
+// let a = 1.5;
+// let b = -1;
+// let c;
+// c = 2;
+
+let name = 'Linh';
+name = "linh";
+
+const PI = 3.14; //Nên đặt chữ hoa
+
+let isHandsome = true;
+isHandsome = false;
+
+let laDepTrai = true;
+laDepTrai = false;
+
+// Kiểu dữ liệu được xác định khi nào => Khi gán giá trị cho biến đấy;
+
+let x = 4;
+++x; //<=> x = x + 1; x = 4 + 1;
+console.log(x);// 5
+
+let y = 6;
+--y; // <=> y = y - 1; y = 6 - 1;
+console.log(y); // 5
+
+
+let a = '3' + 2 + 7; // '32' + 7 => '327'
+// alert('3' + 2 + 7)
+// alert(a);
+
+let b = '3' + (2 + 7); // '3' + 9 => '39'
+
+let c = '3' + (3 + 4) + '5'; // 375
+
+
+let e = 7 - '3' % 2 // 6
+
+let a1 = 1;
+let a2 = '1';
+// alert(a1 == a1) // True
+// alert(a1 == a2) //True
+// alert(a1 != a2) // a1 khác a2 à ? False
+// alert(a1 > a2) //False
+// alert(a1 >= a2) //True
+// alert(a1 < a2 ) // False
+// alert(a1 <= a2) //True: Nhỏ hơn hoặc bằng
+
+//alert(a1 === a2) //True  // ===: false sẽ so sánh cả kiểu dữ liệu
+//alert(a1 !== a2); // False // !== : True
+
